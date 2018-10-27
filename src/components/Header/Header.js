3fd3098ed1bc2c274b0c44d2bc8b0bd0ac73bbe2
bel7aG <belhassen.gharsallah@isetrades.com>
2018-10-28 01:21:38 +0200

    organization

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+  <div className="header">
+    <a href="./php.php" target="_blank">PHP file</a>
+  </div>
+);
+
+export default Header;
