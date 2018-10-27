a59efad4f1e8882d3e6f9681ff5e3afe4a965822
bel7aG <belhassen.gharsallah@isetrades.com>
2018-10-28 01:07:12 +0200

    Updates

---
+++ precache-manifest.9f1d3b456ab1f137aa0780879c160cc2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "984e55918c8eed560303",
+    "url": "/react-php-learning/static/js/runtime~main.984e5591.js"
+  },
+  {
+    "revision": "a8ea5a2b443e36bf0241",
+    "url": "/react-php-learning/static/js/main.a8ea5a2b.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/react-php-learning/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "dff088ed6b25d458e4bc3d1638c86ec4",
+    "url": "/react-php-learning/index.html"
+  }
+];
